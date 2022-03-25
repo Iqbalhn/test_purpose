49487c978256b9b9e4fddc9de6de6374fdfc314c
Iqbalhn <101398722+Iqbalhn@users.noreply.github.com>
2022-03-25 15:07:29 +0600

    test purpose commit

---
+++ app.js
@@ -0,0 +1,59 @@
+// var i;
+
+// function hello(){
+//     for( i =0; i<10;i++){
+//         // console.log(i);
+//     }
+//     console.log(i);
+// }
+// hello();
+
+// convarted method()
+
+// var num = "10.22";
+// var sum = Number(num)
+// console.log(typeof sum);
+// console.log(sum);
+
+// var sum ="10.22";
+// var convart = (parseInt(sum));
+// console.log(typeof convart);
+// console.log(convart);
+
+// var sum ="10.22";
+// var convart = (parseFloat(sum));
+// console.log(typeof convart);
+// console.log(convart);
+
+// var practiceS = 100;
+// var convarted =practiceS.toString();
+// console.log(typeof convarted);
+
+//   pricle1 = 10;
+//   pricle2 = 20;
+//   total = pricle1 + pricle2;
+//   console.log(total);
+// array push method
+//  const cars= ['iqbal','hasan','kamal'];
+//  cars.push='shihab';
+//  console.log(cars); 
+
+// Booleans data type;
+ var fastNumber = 10;
+ var secondNumber = 10;
+ var lastNumber = 30;
+  console.log(fastNumber == secondNumber);
+  console.log(secondNumber == lastNumber);
+
+
+//array object
+  const persone = [
+      {name:"iqbal",age: 22},
+      {name:"hasan",age:30}
+  ];
+  for(let i = 0; i<persone.length; i++){
+      console.log(persone[i].name);
+  }
+
+
+
